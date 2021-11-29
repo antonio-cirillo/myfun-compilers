cd709c731da80006ba687e92b9a3eedaae128a89
Antonio CIrillo <dev.cirillo@gmail.com>
2021-11-29 14:43:45 +0100

    update main test

--- fun-lang-jflex-and-jcup/src/Tester.java
+++ fun-lang-jflex-and-jcup/src/Tester.java
@@ -1,17 +1,24 @@
-import java.io.FileNotFoundException;
+import javax.swing.*;
+import javax.swing.tree.DefaultMutableTreeNode;
+import java.awt.*;
 import java.io.FileReader;
 
 public class Tester {
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws Exception {
 
         parser p = new parser(new Yylex(new FileReader(
-                System.getProperty("user.dir") + "\\test_files\\myfun_program.txt")));
-        try {
-            p.debug_parse();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+                System.getProperty("user.dir") + "\\test_files\\" + args[0])));
+
+        DefaultMutableTreeNode root = (DefaultMutableTreeNode) p.parse().value;
+        JTree tree = new JTree(root);
+        JScrollPane scrollPane = new JScrollPane(tree);
+        JFrame frame = new JFrame("Abstract Syntax Tree");
+        frame.getContentPane().add(scrollPane, BorderLayout.CENTER);
+        frame.setSize(800, 500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
 
     }
+
 }
